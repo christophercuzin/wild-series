c04e6cf845400d775ddf0098066a61f7d4c4f81e
christopher cuzin <ed.ed1009@hotmail.fr>
2022-05-25 09:39:24 +0200

    add data and relationship between program and category

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'action',
+        'aventure',
+        'anime',
+        'fantastique',
+        'Horreur',
+        'comedie'
+
+    ];
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::CATEGORIES as $key => $categoryName) {
+        $category = new Category();
+
+        $category->setName($categoryName);
+
+        $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+}
